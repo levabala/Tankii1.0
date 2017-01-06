7d1bcee1f7f4a04b8f28f805f3d14bbf83eb6dbc
levabala <levabala@ya.ru>
2017-01-06 15:51:20 +0300

    Revert "Revert "v0.1.4""
    This reverts commit 6bf87dc85ed055ac5f65377d5906c4f6a4f098c0.

--- ClientSide/main.js
+++ ClientSide/main.js
@@ -1,1 +1,5 @@
-var connector = new SignalingServerConnector(SIGNALING_SERVER)
+var connector = new SignalingServerConnector(SIGNALING_SERVER, {nick: 'anonymous'}, ['ChatRoom'])
+
+connector.addEventListener('adoptedByHost', function(host){
+  console.warn('Adopted by Host', host.userdata.nick)
+})
