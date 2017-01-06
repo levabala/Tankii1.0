d0b16a2e77569a5356d6436c5c4f99122aa8ab0d
levabala <levabala@ya.ru>
2017-01-06 15:21:15 +0300

    v0.1.4
    SignalingServer API

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+var ERRORS = {
+  0: 'Room already created',
+  1: 'Invalid room name'
+}
+
 var os = require('os');
 var PORT = 3030;
 var nodeStatic = require('node-static');
@@ -6,13 +11,13 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var countplayers = 0;
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendfile(__dirname + '/Client/index.html');
 });
 
 app.use('/', express.static(__dirname + '/Client'))
 
-http.listen(PORT, function(){
+http.listen(PORT, function() {
   console.log('listening on *:' + PORT);
 });
 
@@ -21,107 +26,152 @@ var channels = {};
 var creators = {};
 var sockets = {};
 
-io.sockets.on('connection', function (socket) {
-    //console.log(channels)
-    socket.channels = {};
-    sockets[socket.id] = socket;
+io.sockets.on('connection', function(socket) {
+  socket.channels = {};
+  sockets[socket.id] = socket;
+  sendServersList();
+
+  console.log("[" + socket.id + "] connection accepted");
+  socket.on('disconnect', function() {
+    for (var channel in socket.channels) {
+      part(channel);
+    }
+    console.log("[" + socket.id + "] disconnected");
+    delete sockets[socket.id];
+  });
+
+  socket.on('getServers', function() {
     sendServersList();
+  });
 
-    console.log("["+ socket.id + "] connection accepted");
-    socket.on('disconnect', function () {
-        for (var channel in socket.channels) {
-            part(channel);
-        }
-        console.log("["+ socket.id + "] disconnected");
-        delete sockets[socket.id];
-        console.log(channels)
-    });
+  function sendServersList() {
+    var list = [];
+    for (var c in channels)
+      list.push(c)
+    socket.emit('Servers', list)
+  }
 
-    socket.on('getServers', function(){
-      sendServersList();
-    });
+  socket.on('getPlayersCount', function() {
+    socket.emit('PlayersCount', Object.keys(sockets).length);
+  });
 
-    function sendServersList(){
-      var list = [];
-      for (var c in channels)
-        list.push(c)
-      socket.emit('Servers', list)
+  socket.on('createRoom', function(config) {
+    var roomName = config.roomName;
+    if (channels[roomName]) {
+      socket.emit('error', ERRORS[0])
+      return;
     }
 
-    socket.on('getPlayersCount', function(){
-      socket.emit('PlayersCount', Object.keys(sockets).length);
-    });
+    channels[channel] = {
+      peers: {
 
-    socket.on('join', function (config) {
-        console.log("Player [" + socket.id + "] join ");
+      },
+      creator: socket.id
+    };
+  });
 
-        console.log("Count of players: " + countplayers);
-        var channel = config.channel;
-        var userdata = config.userdata;
+  socket.on('join', function(config) {
+    var channel = config.channel;
+    var userdata = config.userdata || {};
+    if (!userdata.nick) userdata.nick = socket.id;
+    socket['userdata'] = userdata;
+    console.log(userdata)
 
-        if (channel in socket.channels) {
-            console.log("["+ socket.id + "] ERROR: already joined ", channel);
-            return;
-        }
+    console.log("Player [" + socket.id + "](" + userdata.nick + ") joined ");
+    console.log("Count of players: " + (countplayers + 1));
 
-        if (!(channel in channels)) {
-            channels[channel] = {peers: {}, creator: socket.id};
-            socket.emit('joinedRoom');
-            console.log('New Channel Created. Creator: ', socket.id)
-        }
-        else socket.emit('joinedRoom', {hostId: channels[channel].creator});
-
-        for (id in channels[channel].peers) {
-            channels[channel].peers[id].emit('addPeer', {'peer_id': socket.id, 'should_create_offer': false});
-            socket.emit('addPeer', {'peer_id': id, 'should_create_offer': true, 'creator': channels[channel].creator});
-        }
-
-        channels[channel].peers[socket.id] = socket;
-        socket.channels[channel] = channel;
-    });
-
-    function part(channel) {
-        console.log("["+ socket.id + "] part ");
-
-        if (!(channel in socket.channels)) {
-            console.log("["+ socket.id + "] ERROR: not in ", channel);
-            return;
-        }
-
-        delete socket.channels[channel];
-        delete channels[channel].peers[socket.id];
-
-        if (socket.id == channels[channel].creator){
-          console.log('Channel [',channel,'] has been closed')
-          for (id in channels[channel].peers) {
-              channels[channel].peers[id].emit('RoomClosed');
-              delete sockets[id].channels[channel];
-            }
-          delete channels[channel];
-          return;
-        }
-
-        for (id in channels[channel].peers) {
-            channels[channel].peers[id].emit('removePeer', {'peer_id': socket.id});
-            socket.emit('removePeer', {'peer_id': id});
-            return;
-        }
-
-        if (Object.keys(channels[channel].peers).length == 0)
-          delete channels[channel];
+    if (channel in socket.channels) {
+      console.log("[" + socket.id + "] ERROR: already joined ", channel);
+      return;
     }
-    socket.on('part', part);
 
-    socket.on('relayICECandidate', function(config) {
-        var peer_id = config.peer_id;
-        var ice_candidate = config.ice_candidate;
-        if (peer_id in sockets)
-            sockets[peer_id].emit('iceCandidate', {'peer_id': socket.id, 'ice_candidate': ice_candidate});
-    });
-    socket.on('relaySessionDescription', function(config) {
-        var peer_id = config.peer_id;
-        var session_description = config.session_description;
-        if (peer_id in sockets)
-            sockets[peer_id].emit('sessionDescription', {'peer_id': socket.id, 'session_description': session_description});
-    });
+    if (!(channel in channels)) {
+      if (channel in sockets) {
+        socket.emit('error', ERRORS[1])
+        return;
+      }
+      channels[channel] = {
+        peers: {},
+        creator: socket
+      };
+      socket.emit('roomCreated', {roomName: channel});
+      console.log('New Channel Created. Creator: ', socket.id)
+    } else socket.emit('joinedRoom', {
+      'peer_id': channels[channel].creator.id,
+      'should_create_offer': true,
+      'userdata': channels[channel].creator.userdata,
+      'roomName': channel
+    });    
+
+    for (id in channels[channel].peers) {
+      channels[channel].peers[id].emit('addPeer', {
+        'peer_id': socket.id,
+        'should_create_offer': false,
+        'userdata': userdata
+      });
+      socket.emit('addPeer', {
+        'peer_id': id,
+        'should_create_offer': true,
+        'userdata': channels[channel].peers[id].userdata
+      });
+    }
+
+    channels[channel].peers[socket.id] = socket;
+    socket.channels[channel] = channel;
+  });
+
+  function part(channel) {
+    console.log("[" + socket.id + "] part ");
+
+    if (!(channel in socket.channels)) {
+      console.log("[" + socket.id + "] ERROR: not in ", channel);
+      return;
+    }
+
+    delete socket.channels[channel];
+    delete channels[channel].peers[socket.id];
+
+    if (socket.id == channels[channel].creator) {
+      console.log('Channel [', channel, '] has been closed')
+      for (id in channels[channel].peers) {
+        channels[channel].peers[id].emit('RoomClosed');
+        delete sockets[id].channels[channel];
+      }
+      delete channels[channel];
+      return;
+    }
+
+    for (id in channels[channel].peers) {
+      channels[channel].peers[id].emit('removePeer', {
+        'peer_id': socket.id
+      });
+      socket.emit('removePeer', {
+        'peer_id': id
+      });
+      return;
+    }
+
+    if (Object.keys(channels[channel].peers).length == 0 && channel != 'OfficialRoom' && channel != 'ChatRoom')
+      delete channels[channel];
+  }
+  socket.on('part', part);
+
+  socket.on('relayICECandidate', function(config) {
+    var peer_id = config.peer_id;
+    var ice_candidate = config.ice_candidate;
+    if (peer_id in sockets)
+      sockets[peer_id].emit('iceCandidate', {
+        'peer_id': socket.id,
+        'ice_candidate': ice_candidate
+      });
+  });
+  socket.on('relaySessionDescription', function(config) {
+    var peer_id = config.peer_id;
+    var session_description = config.session_description;
+    if (peer_id in sockets)
+      sockets[peer_id].emit('sessionDescription', {
+        'peer_id': socket.id,
+        'session_description': session_description
+      });
+  });
 });
